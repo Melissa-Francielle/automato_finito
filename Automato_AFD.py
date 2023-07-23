c170dc8f58ef749cfa019a3de1c87ddc80bbaa79
Melissa Francielle <129883268+Melissa-Francielle@users.noreply.github.com>
2023-07-23 01:21:25 -0300

    Update Automato_AFD.py

--- Automato_AFD.py
+++ Automato_AFD.py
@@ -1,44 +1,72 @@
-class Automato: #cria uma classe para que depois seja chamada em outros programas
-    def _init_ (self): #utilizando a função para iniciar os objetos, utilizando o método init
-     self.aut_estados = [] #lista
-     self.aut_alfabeto = [] #lista
-     self.aut_transicao = {} #dicionario
-     self.estado_inicial = "" #string
-     self.estado_finais = []  #lista
-    #função self para referenciar o objeto e os atributos do objeto 
+import json 
+import csv
+import time 
+import sys 
 
+class AutomatoDFA:
+    def __init__(self, file_aut):
+        #inicia os paramentros recebendo uma lista com string com nome dos estados dentro do arquivo json 
+        self._initial_states = file_aut["initial"] #estado inicial 
+        self._final_states = file_aut["final"] #estado final
+        self._transitions = file_aut["transitions"] #e as transições aceitas
+        self.result = []
 
-    def estados_repetidos (self, armazena):
-        vetor = [] #cria um vetor para armazenar os dados de armazena
-        for i in armazena: #a função for para percorrer por armazena
-           if (i not in vetor): #com a condição caso i não esteja em vetor
-              vetor.append(i) #então utiliza-se o método append para fazer um "linkagem" do valor aos outro dentro daquela lista de valores
-        return vetor #retornar vetor sendo um novo conjunto
+    def operation (self, str_in_input): #uma função para executar o automato. a partir de uma entrada 
+        current = self._initial_states #estado atual que seria o inicial, para começar o automato 
+        for word in str_in_input: #passa interando em entrada que é (Str_entrada)
+            next = self._get_next (current, word) #a partir de uma variavel proximo_estado é pego o método proximo_estado privado e coloca como atributos a palavra e o estado atual            
+            if next in self._final_states: #caso o proximo estado esteja vázio após percorrer a palavra e não levar ao estado final desejado 
+                self.result.append('Aceita')
+            else:
+                self.result.append('Rejeita')    
     
-    def set_estados (self, aut_estados):
-       self.aut_estados = self.estados_repetidos(aut_estados) #chama a função para verificar se existem estados repetidos 
-       self.aut_estados.sort() #e utiliza a método sort para alterar a lista 
+    def _get_next (self, estado_atual, palavra): #o método chamado logo acima, que pega o estado e tem os mesmos parametros usado acima
+        for transition in self._transitions: #intera transições em self.transições
+            if transition["from"] == estado_atual and (transition["read"] == palavra): #caso a transição from, no caso o arquivo dentro do json
+                #for == ao estado atual que no método acima foi colocado como proximo, e for lido uma palavra ou seja lido um vazio
+                self.operation(palavra, estado_atual) #então retonar para qual proximo estado vai
+        return None
 
-    def entrada (self, aut_estados, estado_inicial, estado_finais, delta):
-        self.aut_estados = aut_estados(', ')
-        self.estado_finais = estado_finais.split(', ')
-
-    def initial_states(self, state): #verificação para ver se o estado inicial colocado faz parte do conjunto de estados   
-        if state in self.aut_estado: #verifica se estado está nos estados e armazena no estado inicial 
-            self.estado_inicial = state #esse estado armazenado a variavel estado inicial
+    def manipulating (self, string):
+        self.operation(string)
+        if('Accepted' in self.result):
+            self.resultado = ()
+            return 1
         else:
-            print("O estado inicial é inválido") #caso contrário o estado selecionado como inicial será dito como inválido
-
-    def final_states (self, finais):
-       finais = self.estados_repetidos(finais) #chama a função para verificar se há estados finais repetidos    
-       for f in finais: #Com a função for percorre os estados 
-            if f in finais: #Condição se f esta em finais
-                if f in self.aut_estados: #Esta em estados finais
-                    if f not in self.estado_final: #mas não esta em estado final
-                        #então ele será adicionado com o método append de lista para ser armazenado nos estados finais
-                        self.estado_final.append(f)
-                    
-    def delta (self, states, q, w):
-        if states in self.t[q][w]:
-            self.fita_automato 
+            return 0
+            
+def automata_file(file_path): #uma função fora da class para ler o arquivo json
+    with open(file_path) as file:
+        return json.load(file)
     
+def cases (file_path):
+    test = []
+    with open (file_path, 'r', newline='') as file:
+        reader = csv.reader (file, delimiter=';')
+        next(reader)
+        for line in reader:
+            if len(line) >= 2:
+                test.append((line[0], int(line[1])))
+    return test
+    
+def main (file_aut_path, file_teste_path, file_out_path):
+    automata = AutomatoDFA(automata_file(file_aut_path))
+    case_test = cases(file_teste_path)
+
+    with open (file_out_path, 'w', newline='') as csv_file:
+        writing = csv.writer(csv_file, delimiter=';')
+        writing.writerow(["Palavra de entrada; ""Resultado esperado; ""Resultado Obtido; ""Tempo; "])
+        
+        for testing in case_test:
+            str_in_input, expected_result = testing
+            start_time = time.time()
+            result = automata.manipulating(str_in_input)
+            end_time = time.time()
+            execution_time = round((end_time - start_time), 10)
+            writing.writerow([str_in_input, expected_result, int(result), float(execution_time)])
+
+if __name__ == "__main__":
+    if len(sys.argv) !=4:
+        print("Ordem para acessar o arquivo: python arquivo tipo ->(.py) arquivo tipo ->(.aut) arquivo do tipo-> (.csv) nome do arquivo de saida tipo -> (.out)")
+    else:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])

a390e0b00525dce2e1cc2ef8308f26dd8ed5bdb4
Joana Shizu Ono <113473493+JoanaShizu@users.noreply.github.com>
2023-07-26 06:31:48 -0300

    Arrumado Automato_e_AFND.py

--- e-AFND/Automato_e_AFND.py
+++ e-AFND/Automato_e_AFND.py
@@ -11,57 +11,45 @@ class Automato_epsilon:
         self._initial_states = set(initial_states)
         self._final_states = file_aut["final"]
         self._transitions = file_aut["transitions"]
-        self.result = []
 
-    #def operation(self, str_in_input):
-     #   current_states = self._initial_states
-      #  for word in str_in_input:
-       #     current_states = self._get_next_states(current_states, word)
-        #    if not current_states: # Adiciona esta condição para interromper a execução se não houver transição para a palavra lida
-         #       break
-        #if any(state in self._final_states for state in current_states):
-        #    return 'A'
-        #else:
-         #   return 'R'
+    def operation(self, str_in_input):
+        current_states = self.epsilon_closure(self._initial_states)
+        for word in str_in_input:
+            current_states = self._get_next_states(current_states, word)
+            current_states = self.epsilon_closure(current_states)
+
+        if any(state in self._final_states for state in current_states):
+            return 1
+        else:
+            return 0
 
     def _get_next_states(self, current_states, word):
         next_states = set()
-        for transition in self._transitions:
-            if transition["from"] == current_states and transition["read"] == word or transition["read"] == "&":
-                if isinstance(transition["to"], int):
+        for state in current_states:
+            for transition in self._transitions:
+                if transition["from"] == state and transition["read"] == word:
                     next_states.add(transition["to"])
-                else:
-                    next_states.update(transition["to"])       
+
         return list(next_states)
         
     def epsilon_closure(self, states):
         closure = set(states)
         stack = list(states)
         while stack:
-            states = stack.pop()
-            epsilon_transitions = self._get_next_states({states}, {"&"})
+            state = stack.pop()
+            epsilon_transitions = self._get_epsilon_transitions(state)
             for next_state in epsilon_transitions:
                 if next_state not in closure:
                     closure.add(next_state)
-                    stack.extend([next_state])
+                    stack.append(next_state)
         return list(closure)
-
-    def manipulating(self, string):
-        self.result = [] # Limpa os resultados anteriores antes de executar o autômato
-        current_states = self.epsilon_closure(set(self._initial_states))
-        for word in string:
-            next_states = set(current_states)
-            for state in current_states:
-                state_transitions = self._get_next_states(set([state]), word)
-                next_states |= set(state_transitions) #O |= atribuirá o valor após executar um OR entre duas variáveis, mas bit a bit
-            current_states = self.epsilon_closure(next_states)  # Usamos set() para tratar os estados como um conjunto
-            if not current_states:
-                break
-        if any(state in self._final_states for state in current_states):
-            return '1'
-        else:
-            return '0'
-
+        
+    def _get_epsilon_transitions(self, state):
+        epsilon_transitions = set()
+        for transition in self._transitions:
+            if transition["from"] == state and transition["read"] == "c":
+                epsilon_transitions.add(transition["to"])
+        return epsilon_transitions
 
 def automata_file(file_path):
     with open(file_path) as file:
@@ -69,13 +57,13 @@ def automata_file(file_path):
 
 def cases(file_path):
     test = []
-    with open(file_path, 'r', newline='') as file:
+    with open(file_path, 'r', newline='', encoding='utf-8-sig') as file:
         reader = csv.reader(file, delimiter=';')
         next(reader)
         for line in reader:
             if len(line) >= 2:
                 str_in_input = line[0]
-                expected_result = int(''.join(filter(str.isdigit, line[1].strip())))
+                expected_result = int(line[1].strip())
                 test.append((str_in_input, expected_result))
     return test
 
@@ -87,10 +75,9 @@ def main(file_aut_path, file_teste_path, file_out_path):
         writing = csv.writer(csv_file, delimiter=';')
         writing.writerow(["Palavra de entrada", "Resultado esperado", "Resultado obtido", "Tempo"])
 
-        for testing in case_test:
-            str_in_input, expected_result = testing
+        for str_in_input, expected_result in case_test:
             start_time = time.perf_counter()
-            result = automata.manipulating(str_in_input)
+            result = automata.operation(str_in_input)
             end_time = time.perf_counter()
 
             execution_time = "{:.5f}".format(end_time - start_time)  # Formatação com cinco casas decimais
